0f55a27e67cc02240788d77a946af166174c210a
lpeng <3443002624@qq.com>
2022-05-19 16:56:11 +0800

    Add node and debugger test

--- test.js
+++ test.js
@@ -1,1 +1,29 @@
-console.log(2*0)
+Object.prototype.map = function (handleFn, thisValue) {
+  const obj = this;
+  let res = {};
+  for (let prop in obj) {
+    if (obj.hasOwnProperty(prop)) {
+      res[prop] = handleFn.call(thisValue, obj[prop], prop, obj);
+    }
+  }
+  return res;
+};
+// 测试用例
+var obj = {
+  name: "sunny",
+  sex: "man",
+};
+var res = obj.map(
+  //   function (val, prop, obj) {
+  //     console.log(this);
+  //     console.log(val, prop, obj);
+  //     return prop + "--" + val;
+  //   },
+  (val, prop, obj) => {
+    console.log(this);
+    console.log(val, prop, obj);
+    return prop + "--" + val;
+  },
+  { name: "thisthis" }
+);
+console.log("res:", res, Object);
