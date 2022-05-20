96eb008998cb1db83c15272f9d072a462ad3bd1d
lpeng <3443002624@qq.com>
2022-05-20 17:17:55 +0800

    Add node and debugger test

--- Node/app.js
+++ Node/app.js
@@ -1,14 +1,58 @@
-const http = require('http');
+const http = require("http");
+const fs = require("fs");
 
-const hostname = '127.0.0.1';
+const fileStr = "E:\\project\\snacks\\Node\\text.txt";
+const buf = fs.readFileSync(fileStr);
+const data = fs.readFileSync(fileStr, "utf8");
+const hostname = "127.0.0.1";
 const port = 3000;
-
 const server = http.createServer((req, res) => {
+  console.log(req.url);
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.setHeader("Content-Type", "text/plain");
+  // res.end("Hello World");
+  res.end(data);
 });
-
+const postData = JSON.stringify({
+  msg: "Hello World!",
+});
+const options = {
+  hostname: hostname,
+  port: port,
+  path: "/test",
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Content-Length": Buffer.byteLength(postData),
+  },
+};
+server.on("error", (e) => {
+  if (e.code === "EADDRINUSE") {
+    console.log("Address in use, retrying...");
+    setTimeout(() => {
+      server.close();
+      server.listen(PORT, HOST);
+    }, 1000);
+  }
+});
 server.listen(port, hostname, () => {
+  console.log(buf, data);
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+const req = http.request(options, (res) => {
+  console.log(`STATUS: ${res.statusCode}`);
+  console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+  res.setEncoding("utf8");
+  res.on("data", (chunk) => {
+    console.log(`BODY: ${chunk}`);
+  });
+  res.on("end", () => {
+    console.log("No more data in response.");
+  });
+});
+req.on("error", (e) => {
+  console.error(`problem with request: ${e.message}`);
+});
+// // Write data to request body
+// req.write(postData);
+// req.end();
