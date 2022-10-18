07d6ab22e2b6ced5b9222fd918ebe36cb1c9adf4
Demon <3443002624@qq.com>
2022-10-18 17:27:08 +0800

    Add Fetch api test code

---
+++ Fetch/index.js
@@ -0,0 +1,37 @@
+// *** Fetch *** //
+fetch("https://elm.cangdu.org/v1/pois?city_id=1&keyword=迪士尼&type=search", {
+  credentials: "include",
+  method: "GET",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+  mode: "cors",
+  cache: "force-cache",
+})
+  .then((result) => {
+    return result;
+  })
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((reason) => {
+    console.error("Error...", reason);
+  });
+
+// *** XMLHttpRequest *** //
+// https://developer.mozilla.org/zh-CN/docs/Web/API/ProgressEvent
+const onLoad = function (pe) {
+  // XMLHttpRequest 的 response 属性返回响应的正文。返回的类型为
+  // ArrayBuffer、Blob、Document、JavaScript Object 或字符串中的一个。这取决于请求的 responseType 属性
+  // https://developer.mozilla.org/zh-CN/docs/Web/API/XMLHttpRequest/response
+  console.log(pe, this, this.response);
+};
+
+var xhr = new XMLHttpRequest();
+xhr.addEventListener("load", onLoad);
+xhr.open(
+  "GET",
+  "https://elm.cangdu.org/v1/pois?city_id=1&keyword=迪士尼&type=search"
+);
+xhr.send();
