c1db9c357005ddc45b40e43ce99e4adf929b04b7
Demon <3443002624@qq.com>
2024-05-24 10:18:11 +0800

    Update

--- Node/T.js
+++ Node/T.js
@@ -1,7 +1,11 @@
 const origin = 98.46;
 const n = 1;
-const cost1 = origin * 100 * n * (0.5 / 1000);
-const cost2 = origin * 100 < 10000 ? 5 : (origin * 100 * 2.5) / 10000;
+const cost1 = origin * 100 * n * (0.5 / 1000); //yin hua
+const cost2 = origin * 100 < 10000 ? 5 : (origin * 100 * 2.5) / 10000; //yong jin
 const cost = cost1 + cost2;
-const income = origin * 100 - cost;
-console.log("cost is " + cost + ",must <" + (income / 100).toFixed(2));
+let income = origin * 100 - cost;
+const cost3 = income * 100 < 10000 ? 5 : (income * 100 * 2.5) / 10000;
+income = income - cost3;
+console.log(
+  "cost is " + (cost + cost3) + ",must <" + (income / 100).toFixed(2)
+);
