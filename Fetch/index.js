dfa1da8f14ff01e967df2c1e12dcfabd658f6859
Demon <3443002624@qq.com>
2024-05-30 11:33:33 +0800

    Refactor

--- Fetch/index.js
+++ Fetch/index.js
@@ -10,31 +10,13 @@ fetch("https://elm.cangdu.org/v1/pois?city_id=1&keyword=迪士尼&type=search", 
   cache: "force-cache",
 })
   .then((result) => {
-    console.log('%c ~ result ~ ', 'color:#2ecc71', result)
-    return 'yes';
+    console.log("%c ~ result ~ ", "color:#2ecc71");
+    return "yes";
   })
   .then((result) => {
-    console.log('%c ~ result ~ ', ' color:#2ecc71', result)
+    console.log("%c ~ result ~ ", " color:#2ecc71");
     console.log(result);
   })
   .catch((reason) => {
     console.error("Error...", reason);
   });
-
-// *** XMLHttpRequest *** //
-// https://developer.mozilla.org/zh-CN/docs/Web/API/ProgressEvent
-const onLoad = function (pe) {
-  // XMLHttpRequest 的 response 属性返回响应的正文。返回的类型为
-  // ArrayBuffer、Blob、Document、JavaScript Object 或字符串中的一个。这取决于请求的 responseType 属性
-  // https://developer.mozilla.org/zh-CN/docs/Web/API/XMLHttpRequest/response
-  console.log(pe, this, this.response);
-};
-
-var xhr = new XMLHttpRequest();
-xhr.addEventListener("load", onLoad);
-// TODO 测试后端 api 
-xhr.open(
-  "GET",
-  "https://elm.cangdu.org/v1/pois?city_id=1&keyword=迪士尼&type=search"
-);
-xhr.send();
