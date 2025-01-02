6e089cb6e3bf4d7390fabd810ba3889213f79cac
Demon <3443002624@qq.com>
2025-01-02 16:25:38 +0800

    Update ws client.

--- Node/ws/app.js
+++ Node/ws/app.js
@@ -29,26 +29,26 @@ setTimeout(() => {
   console.log(wss.clients);
 }, 30000);
 
-// client test:
+// // client test:
 
-let count = 0;
+// let count = 0;
 
-let ws = new WebSocket("ws://localhost:3000/ws/chat");
+// let ws = new WebSocket("ws://localhost:3000/ws/chat");
 
-ws.on("open", function () {
-  console.log(`[CLIENT] open()`);
-  ws.send("Hello!");
-});
+// ws.on("open", function () {
+//   console.log(`[CLIENT] open()`);
+//   ws.send("Hello!");
+// });
 
-ws.on("message", function (message) {
-  console.log(`[CLIENT] Received: ${message}`);
-  count++;
-  if (count > 3) {
-    ws.send("Goodbye!");
-    ws.close();
-  } else {
-    setTimeout(() => {
-      ws.send(`Hello, I'm Mr No.${count}!`);
-    }, 1000);
-  }
-});
+// ws.on("message", function (message) {
+//   console.log(`[CLIENT] Received: ${message}`);
+//   count++;
+//   if (count > 3) {
+//     ws.send("Goodbye!");
+//     ws.close();
+//   } else {
+//     setTimeout(() => {
+//       ws.send(`Hello, I'm Mr No.${count}!`);
+//     }, 1000);
+//   }
+// });
