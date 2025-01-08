1ef6e57452957e2ffe3857afdc20c80fde732826
Demon <3443002624@qq.com>
2025-01-08 09:35:43 +0800

    Update T.js

--- Node/T.js
+++ Node/T.js
@@ -5,17 +5,18 @@ const { stdin: input, stdout: output } = require("node:process");
 const rl = readline.createInterface({ input, output });
 let origin = 100;
 
-rl.question("What do you think of Node.js? ", (answer) => {
+rl.question("what(?,?)?", (answer) => {
   // TODO: Log the answer in a database
-  console.log(`Thank you for your valuable  feedback: ${answer}`);
-  origin = Number(answer);
-  const n = 1;
+  console.log(`your origin: ${answer}`);
+  const arr = answer.includes(",") ? answer.split(",") : [];
+  origin = arr.length === 0 ? Number(answer) : Number(arr[0]);
+  const n = arr.length === 0 ? 1 : Number(arr[1]);
   const cost1 = origin * 100 * n * (0.5 / 1000); //yin hua
   const cost2 = origin * 100 < 10000 ? 5 : (origin * 100 * 2.5) / 10000; //yong jin
   const cost = cost1 + cost2;
-  let buy = origin * 100 - cost;
+  let buy = origin * 100 * n - cost;
   const cost3 = buy < 10000 ? 5 : (buy * 2.5) / 10000;
-  buy = buy - cost3;
+  buy = (buy - cost3) / n;
   console.log(
     "cost is " +
       cost +
