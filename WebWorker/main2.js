17696b925d62f732b8c8011ddf39038747c4524e
Demon <3443002624@qq.com>
2025-02-05 15:47:53 +0800

    Delete node Web Worker.

--- WebWorker/main2.js
+++
@@ -1,8 +0,0 @@
-const { MessageChannel, receiveMessageOnPort } = require('worker_threads');
-const { port1, port2 } = new MessageChannel();
-port1.postMessage({ hello: 'world' });
-
-console.log(receiveMessageOnPort(port2));
-// Prints: { message: { hello: 'world' } }
-console.log(receiveMessageOnPort(port2));
-// Prints: undefined
