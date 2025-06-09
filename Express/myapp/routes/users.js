1f07e4f337f174e63f1d104f02676f464d53da44
Demon <demon3443002624@outlook.com>
2025-06-09 09:29:15 +0800

    Update describe.

--- Express/myapp/routes/users.js
+++ Express/myapp/routes/users.js
@@ -1,26 +1,27 @@
-var express = require("express");
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET users listing. */
-router.get("/", function (req, res, next) {
+router.get('/', function (req, res, next) {
   // 函数实现，参数 n 单位 毫秒 ；
   function sleep(n) {
-    var start = new Date().getTime();
+    var start = new Date().getTime()
     while (true) {
       if (new Date().getTime() - start > n) {
         // 使用  break  实现；
-        break;
+        break
       }
     }
   }
 
+  // Update
   // 调用方法，同步执行，阻塞后续程序的执行；
-  sleep(3000);
-  res.send("respond with a resource");
-});
+  sleep(3000)
+  res.send('respond with a resource')
+})
 
-router.get("/getJson", function (req, res, next) {
-  res.json({ a: "respond with a resource", b: 1, c: false, d: "dddd" });
-});
+router.get('/getJson', function (req, res, next) {
+  res.json({ a: 'respond with a resource', b: 1, c: false, d: 'dddd' })
+})
 
-module.exports = router;
+module.exports = router
